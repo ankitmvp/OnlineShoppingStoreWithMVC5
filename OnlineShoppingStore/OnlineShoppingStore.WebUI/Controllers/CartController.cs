dc7fdb5337eba5b31a6a5861e4ea6b0599595512
Ankit Bansal <ankitbansalniit@outlook.com>
2015-10-10 17:51:08 +0530

    Added Shopping Cart

---
+++ OnlineShoppingStore/OnlineShoppingStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStore.Domain.Entities;
+using OnlineShoppingStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel { Cart = GetCart(), ReturnUrl = returnUrl });
+        }
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
+            if (product!=null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
---
+++ OnlineShoppingStore/OnlineShoppingStore.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OnlineShoppingStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
